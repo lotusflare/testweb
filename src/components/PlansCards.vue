29b54a1db26c63c6e13824357bd95bbd1bc4de05
Qiance Yu <qiance.yu@lotusflare.com>
2022-07-13 01:44:16 -0700

    change tab to button group

--- src/components/PlansCards.vue
+++ src/components/PlansCards.vue
@@ -1,19 +1,16 @@
 <template>
     <v-app>
-        <v-sheet>
-            <v-tabs background-color="black" dark centered>
-                <v-tab>1</v-tab>
-                <v-tab>2</v-tab>
-            </v-tabs>
-        </v-sheet>
-
-        <!-- <v-btn-toggle v-model="toggle_exclusive" rounded dark>
-            <v-btn> 1 </v-btn>
-            <v-btn> 2 </v-btn>
-        </v-btn-toggle> -->
-        <v-card-text>
+        <!--center button group-->
+        <v-card-actions class="justify-center mb-5">
+            <v-btn-toggle v-model="toggle_exclusive" rounded mandatory>
+                <v-btn>1</v-btn>
+                <v-btn>2</v-btn>
+            </v-btn-toggle>
+        </v-card-actions>
+        <!-- <v-card-text> -->
+        <v-container class="white">
             <v-row>
-                <v-col cols="12" md="4" xs="12" v-for="(item, index) in plans" :key="index">
+                <v-col cols="12" md="4" xs="12" v-for="(item, index) in plan" :key="index">
                     <v-card>
                         <v-card-title>
                             <v-col align="center">
@@ -34,7 +31,7 @@
                             <v-spacer></v-spacer>
                             <router-link
                                 style="text-decoration: none; color: inherit"
-                                :to="`/checkout?title=${item.title}`"
+                                :to="`/checkout?title=${item.title}&price=${item.price}&lines=${item.lines}`"
                             >
                                 <v-btn color="black" text> Get Plan </v-btn>
                             </router-link>
@@ -42,7 +39,8 @@
                     </v-card>
                 </v-col>
             </v-row>
-        </v-card-text>
+        </v-container>
+        <!-- </v-card-text> -->
     </v-app>
 </template>
 
@@ -52,26 +50,60 @@
         data() {
             return {
                 plans: {
-                    1: {
-                        title: 'Only What You Need',
-                        price: '$19.99',
-                        description:
-                            "Perfect if you're a light data user, connect mostly to WiFi or only use your phone to talk and text.",
+                    line1: {
+                        1: {
+                            title: 'Only What You Need',
+                            lines: 1,
+                            price: '$19.99',
+                            description:
+                                "Perfect if you're a light data user, connect mostly to WiFi or only use your phone to talk and text.",
+                        },
+                        2: {
+                            title: 'Everything You Want',
+                            lines: 1,
+                            price: '$29.99',
+                            description:
+                                "Ideal if you're regularly on your phone browsing, listening to music or streaming videos.",
+                        },
+                        3: {
+                            title: 'Everywhere You Go',
+                            lines: 1,
+                            price: '$39.99',
+                            description:
+                                "The best if you like to travel or if you're away from Wi-Fi. Includes stateside hotspot & global text.",
+                        },
                     },
-                    2: {
-                        title: 'Everything You Want',
-                        price: '$29.99',
-                        description:
-                            "Ideal if you're regularly on your phone browsing, listening to music or streaming videos.",
-                    },
-                    3: {
-                        title: 'Everywhere You Go',
-                        price: '$39.99',
-                        description:
-                            "The best if you like to travel or if you're away from Wi-Fi. Includes stateside hotspot & global text.",
+                    line2: {
+                        1: {
+                            title: 'Only What You Need',
+                            lines: 2,
+                            price: '$29.99',
+                            description:
+                                "Perfect if you're a light data user, connect mostly to WiFi or only use your phone to talk and text.",
+                        },
+                        2: {
+                            title: 'Everything You Want',
+                            lines: 2,
+                            price: '$39.99',
+                            description:
+                                "Ideal if you're regularly on your phone browsing, listening to music or streaming videos.",
+                        },
+                        3: {
+                            title: 'Everywhere You Go',
+                            lines: 2,
+                            price: '$49.99',
+                            description:
+                                "The best if you like to travel or if you're away from Wi-Fi. Includes stateside hotspot & global text.",
+                        },
                     },
                 },
+                toggle_exclusive: 0,
             }
+        },
+        computed: {
+            plan() {
+                return this.toggle_exclusive === 0 ? this.plans.line1 : this.plans.line2
+            },
         },
         methods: {},
         mounted() {
