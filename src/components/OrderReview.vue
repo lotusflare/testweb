6575aeb313a62d9d1bd5470754c0e8f298a22879
hyouzan <qiance.yu@lotusflare.com>
2022-07-13 18:37:01 -0700

    Create OrderReview.vue

---
+++ src/components/OrderReview.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-row no-gutters>
+        <v-col cols="4">
+            <v-img src="../assets/images/onlywhatyouneed-plan-385dfc6572.c4a50b8341.png" height="50px" contain></v-img>
+        </v-col>
+        <v-col cols="8">
+            <div class="heading-6">{{ this.$route.query.title }}</div>
+            <div class="subheading">Get {{ this.$route.query.lines }} line for {{ this.$route.query.price }}</div>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: 'OrderReview',
+    }
+</script>
+
+<style scoped></style>
