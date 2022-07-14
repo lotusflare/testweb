64495b1b28744cce5986ea42d28416ca0f2d4d26
hyouzan <qiance.yu@lotusflare.com>
2022-07-13 18:37:22 -0700

    add global event bus

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import router from './router'
-// import VeeValidate from 'vee-validate'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
-// Vue.use(VeeValidate)
+
 
 new Vue({
     vuetify,
     render: (h) => h(App),
     router,
+    beforeCreate() {
+        Vue.prototype.$bus = this
+    }
 }).$mount('#app')
