969cb378ddf1761478ac84127d72c59aa34e2c69
Qiance Yu <qiance.yu@lotusflare.com>
2022-07-15 01:49:26 -0700

    Create success page

---
+++ src/pages/RwSuccess.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- a submit success page-->
+    <v-app>
+        <AppHeader />
+        <v-main>
+            <v-row>
+                <v-col cols="12" class="text-center">
+                    <h1>Thank you for your order!</h1>
+                    <p>You will receive an email with your order confirmation shortly.</p>
+                    <v-btn color="primary" href="/">Go to Home</v-btn>
+                    
+                </v-col>
+            </v-row>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+    import AppHeader from '@/components/AppHeader'
+    export default {
+        name: 'SubmitSuccess',
+        components: {
+            AppHeader,
+        },
+    }
+</script>
+
+<style scoped></style>
