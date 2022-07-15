17a324731f3345a7c970d86f69beb513a459a469
Qiance Yu <qiance.yu@lotusflare.com>
2022-07-15 01:49:36 -0700

    add success page route

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,29 @@
 import VueRouter from 'vue-router'
 import RwHome from '@/pages/RwHome'
 import RwCheckout from '@/pages/RwCheckout'
+import RwSuccess from '@/pages/RwSuccess'
 
 export default new VueRouter({
     routes: [
+        //home page
+        {
+            path: '/',
+            name: 'RwHome',
+            component: RwHome,
+        },
+        
+        //checkout page
         {
             path: '/checkout',
             name: 'RwCheckout',
             component: RwCheckout,
         },
+
+        //success page
         {
-            path: '/',
-            name: 'RwHome',
-            component: RwHome,
+            path: '/success',
+            name: 'RwSuccess',
+            component: RwSuccess,
         },
     ],
 })
