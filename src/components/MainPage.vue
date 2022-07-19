f8e9ce73e7fd41e72e1cb64d290976fa757e7908
hyouzan <qiance.yu@lotusflare.com>
2022-07-18 18:29:39 -0700

    beautify main page

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-container class="mt-10">
+    <v-container style="max-width: 1200px; position: relative;" class="mt-10">
         <v-row class="text-center">
             <v-col class="mb-4">
                 <h1 class="display-2 mb-3">
                     Pick the plan that's
-                    <span class="display-2 mb-3 font-weight-bol red--text"> right for you </span>
+                    <span class="display-2 mb-3 font-weight-black red--text "> right for you </span>
                 </h1>
 
                 <p class="subheading font-weight-regular">
@@ -15,7 +15,7 @@
             </v-col>
 
             <v-col class="ma-0" cols="12">
-                <h2 class="headline font-weight-bold mb-3">How many lines will be on your plan?</h2>
+                <h2 class="headline font-weight-regular mb-3">How many lines will be on your plan?</h2>
             </v-col>
         </v-row>
     </v-container>
