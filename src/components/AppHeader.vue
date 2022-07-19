23a116338c1d0a9645a71a4083b8ff082623ae31
hyouzan <qiance.yu@lotusflare.com>
2022-07-18 18:29:08 -0700

    Add dashboard and login/logout nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,12 +6,33 @@
         <router-link style="text-decoration: none; color: inherit" to="/">
             <v-btn target="_blank" text> Plans </v-btn>
         </router-link>
+        <router-link v-if="isLoggedin()" style="text-decoration: none; color: inherit" to="/dashboard">
+            <v-btn target="_blank" text> Dashboard </v-btn>
+        </router-link>
+        <v-spacer></v-spacer>
+        <router-link v-if="!isLoggedin()" style="text-decoration: none; color: inherit" to="/login">
+            <v-btn target="_blank" text> Login </v-btn>
+        </router-link>
+        <!-- <router-link v-else style="text-decoration: none; color: inherit" to="/"> -->
+        <v-btn v-else target="_blank" text @click="logout"> Logout </v-btn>
+        <!-- </router-link> -->
     </v-app-bar>
 </template>
 
 <script>
     export default {
         name: 'AppHeader',
+        methods: {
+            isLoggedin() {
+                return localStorage.getItem('api_token') !== null
+            },
+            logout() {
+                localStorage.removeItem('api_token')
+                localStorage.removeItem('user_id')
+                // redirect to login page with reload
+                this.$router.go('/')
+            },
+        },
     }
 </script>
 
