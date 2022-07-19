405d453ef60ed099f5a4d8500de185c7a39176ce
hyouzan <qiance.yu@lotusflare.com>
2022-07-18 18:30:16 -0700

    Beautify plans cards

--- src/components/PlansCards.vue
+++ src/components/PlansCards.vue
@@ -2,40 +2,63 @@
     <v-app>
         <!--center button group-->
         <v-card-actions class="justify-center mb-5">
-            <v-btn-toggle v-model="toggle_exclusive" rounded mandatory>
+            <v-btn-toggle v-model="toggle_exclusive" rounded mandatory dark>
                 <v-btn>1</v-btn>
                 <v-btn>2</v-btn>
             </v-btn-toggle>
         </v-card-actions>
         <!-- <v-card-text> -->
-        <v-container class="white">
+        <v-container fluid style="max-width: 1200px; position: relative;" class="white">
             <v-row>
                 <v-col cols="12" md="4" xs="12" v-for="(item, index) in plan" :key="index">
-                    <v-card>
+                    <div class="black darken-2 text-center">
+                        <span class="white--text">{{ item.description }}</span>
+                    </div>
+                    <v-card height="800px" class="d-flex flex-column">
                         <v-card-title>
                             <v-col align="center">
-                                <div class="text-h6">
-                                    {{ item.title }}
+                                <div>
+                                    {{ item.name }}
                                 </div>
-                                <div class="text-h4">
-                                    {{ item.price }}
+                                <div v-if="toggle_exclusive === 0" class="text-h3 font-weight-black">
+                                    $ {{ item.DISH_Customer_Price }}
+                                </div>
+                                <div v-else-if="toggle_exclusive === 1" class="text-h3 font-weight-black">
+                                    $ {{ item.DISH_Customer_Price * 2 }}
+                                </div>
+                                <div v-if="toggle_exclusive === 0" class="subtitle-2">PER LINE FOR DISH CUSTOMERS</div>
+                                <div v-else-if="toggle_exclusive === 1" class="subtitle-2">
+                                    FOR TWO LINES FOR DISH CUSTOMERS
                                 </div>
                             </v-col>
                         </v-card-title>
+                        <v-divider class="mx-4"></v-divider>
                         <v-card-text>
-                            <div class="subheading">
-                                {{ item.description }}
-                            </div>
+                            <v-col align="center">
+                                <div class="subheading">
+                                    {{ item.Marketing_Description }}
+                                </div>
+                                <div class="mt-4 text--primary">
+                                    {{ item.Plan_Features }}
+                                </div>
+                            </v-col>
                         </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <!-- <router-link
-                                style="text-decoration: none; color: inherit"
-                                :to="`/checkout?title=${item.title}&price=${item.price}&lines=${item.lines}`"
-                            > -->
-                            <v-btn color="black" text @click="getPlan(item)"> Get Plan </v-btn>
-                            <!-- </router-link> -->
+                        <v-spacer></v-spacer>
+                        <v-card-actions class="flex-end">
+                            <v-col align="center">
+                                <v-btn color="black" rounded dark @click="getPlan(item)"> Get Plan </v-btn>
+                            </v-col>
                         </v-card-actions>
+                        <v-card-text class="my-0">
+                            <v-col align="center">
+                                <div v-if="toggle_exclusive === 0" class="subheading">
+                                    Retail Price: {{ item.Retail_Price }}
+                                </div>
+                                <div v-else-if="toggle_exclusive === 1" class="subheading">
+                                    Retail Price: {{ item.Retail_Price * 2 }}
+                                </div>
+                            </v-col>
+                        </v-card-text>
                     </v-card>
                 </v-col>
             </v-row>
@@ -45,81 +68,60 @@
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
         name: 'PlansCards',
         data() {
             return {
-                plans: {
-                    line1: {
-                        1: {
-                            title: 'Only What You Need',
-                            lines: 1,
-                            price: '$19.99',
-                            description:
-                                "Perfect if you're a light data user, connect mostly to WiFi or only use your phone to talk and text.",
-                        },
-                        2: {
-                            title: 'Everything You Want',
-                            lines: 1,
-                            price: '$29.99',
-                            description:
-                                "Ideal if you're regularly on your phone browsing, listening to music or streaming videos.",
-                        },
-                        3: {
-                            title: 'Everywhere You Go',
-                            lines: 1,
-                            price: '$39.99',
-                            description:
-                                "The best if you like to travel or if you're away from Wi-Fi. Includes stateside hotspot & global text.",
-                        },
-                    },
-                    line2: {
-                        1: {
-                            title: 'Only What You Need',
-                            lines: 2,
-                            price: '$29.99',
-                            description:
-                                "Perfect if you're a light data user, connect mostly to WiFi or only use your phone to talk and text.",
-                        },
-                        2: {
-                            title: 'Everything You Want',
-                            lines: 2,
-                            price: '$39.99',
-                            description:
-                                "Ideal if you're regularly on your phone browsing, listening to music or streaming videos.",
-                        },
-                        3: {
-                            title: 'Everywhere You Go',
-                            lines: 2,
-                            price: '$49.99',
-                            description:
-                                "The best if you like to travel or if you're away from Wi-Fi. Includes stateside hotspot & global text.",
-                        },
-                    },
-                },
+                plans: [],
                 toggle_exclusive: 0,
             }
         },
         computed: {
+            // filter the plans line number based on the toggle_exclusive value
+            // toggle_exclusive should plus 1 to get the line number
             plan() {
-                return this.toggle_exclusive === 0 ? this.plans.line1 : this.plans.line2
+                return this.plans.filter((item) => item.num_of_lines === this.toggle_exclusive + 1)
             },
+            // when num_of_lines is 2, the 2 lines price should be times 2
         },
         methods: {
             getPlan(item) {
-                console.log(item.title)
+                console.log(item)
                 this.$router.push({
                     name: 'RwCheckout',
-                    params: {
-                        title: item.title,
-                        price: item.price,
-                        lines: item.lines,
+                    query: {
+                        title: item.name,
+                        price: item.DISH_Customer_Price,
+                        // lines: item.lines,
+                        lines: item.num_of_lines,
                     },
                 })
             },
         },
         mounted() {
-            // console.log('PlansCards mounted')
+            axios
+                .post('https://api-project9.lotusflare.com/api/v3/catalog/get_offers', {
+                    operator_name: 'dishb2b',
+                })
+                .then((response) => {
+                    console.log(response.data)
+                    Object.entries(response.data.offer_by_id).forEach((element) => {
+                        // if it is a plan then add to plans
+                        if (element[1].data.DISH_Customer_Price) {
+                            this.plans.push(element[1].data)
+                        }
+                    })
+                    // sort the plans by price
+                    this.plans.sort((a, b) => {
+                        return a.DISH_Customer_Price - b.DISH_Customer_Price
+                    })
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            console.log(this.plans)
+            // console.log(typeof this.plans[0].DISH_Exclusive_Price)
         },
     }
 </script>
