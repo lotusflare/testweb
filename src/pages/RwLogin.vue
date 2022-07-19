26c69a329f0841e6595de2a6ea89722cdb9076ed
hyouzan <qiance.yu@lotusflare.com>
2022-07-18 18:30:23 -0700

    Create RwLogin.vue

---
+++ src/pages/RwLogin.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- vuetify login page -->
+    <v-app>
+        <AppHeader />
+        <v-main>
+           <AccountLogin />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+    import AppHeader from '@/components/AppHeader'
+    import AccountLogin from '@/components/AccountLogin'
+    export default {
+        name: 'RwLogin',
+        components: {
+            AppHeader,
+            AccountLogin,
+        },
+    }
+</script>
+
+<style scoped></style>
