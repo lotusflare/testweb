ca102193435393eca91f9ffdf70e0e51ab52483a
hyouzan <qiance.yu@lotusflare.com>
2022-07-18 18:30:21 -0700

    Create RwDashboard.vue

---
+++ src/pages/RwDashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+        <AppHeader />
+        <!-- show user info -->
+        <v-main>
+            <MyAccount />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+    import AppHeader from '../components/AppHeader'
+    import MyAccount from '../components/MyAccount'
+    export default {
+        name: 'RwDashboard',
+        components: {
+            AppHeader,
+            MyAccount,
+        },
+    }
+</script>
+
+<style scoped></style>
