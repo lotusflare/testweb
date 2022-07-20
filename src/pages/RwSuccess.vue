b391e97d68b4928143faceef034dcee31c2fcd26
Qiance Yu <qiance.yu@lotusflare.com>
2022-07-19 23:51:54 -0700

    Go to dashboard

--- src/pages/RwSuccess.vue
+++ src/pages/RwSuccess.vue
@@ -7,8 +7,8 @@
                 <v-col cols="12" class="text-center">
                     <h1>Thank you for your order!</h1>
                     <p>You will receive an email with your order confirmation shortly.</p>
-                    <router-link style="text-decoration: none; color: inherit" to="/">
-                        <v-btn color="primary">Go to Home</v-btn>
+                    <router-link style="text-decoration: none; color: inherit" to="/dashboard">
+                        <v-btn color="primary">Go to Dashboard</v-btn>
                     </router-link>
                 </v-col>
             </v-row>
