29d10e7e4901ef496607b8310885edae6a0e989d
hyouzan <qiance.yu@lotusflare.com>
2022-07-20 13:55:09 -0700

    add some methods

--- src/components/MyAccount.vue
+++ src/components/MyAccount.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <v-card v-if="loggedIn" class="mx-auto" max-width="1200" elevation="0">
-            <v-card-title>
-                <p class="text-h4 font-weight-bold">My Account</p>
+        <v-card v-if="loggedIn" class="mt-10 mx-auto align-center" max-width="800" elevation="0">
+            <v-card-title class="justify-center">
+                <p class="text-h4 font-weight-bold">Account information</p>
             </v-card-title>
             <!-- <v-card-text> -->
             <v-list-item two-line>
@@ -11,6 +11,7 @@
                     <v-list-item-subtitle>{{ userId }}</v-list-item-subtitle>
                 </v-list-item-content>
             </v-list-item>
+            <!-- FIXME: no name data -->
             <!-- <v-list-item-content>
                     <v-list-item-title>First name</v-list-item-title>
                     <v-list-item-subtitle>No data yet</v-list-item-subtitle>
@@ -37,13 +38,13 @@
                     <v-list-item two-line>
                         <v-list-item-content>
                             <v-list-item-title>Address</v-list-item-title>
-                            <v-list-item-subtitle>{{ address }}</v-list-item-subtitle>
+                            <!-- <v-list-item-subtitle>{{ address }}</v-list-item-subtitle> -->
+                            <v-list-item-subtitle>{{ address.address }}</v-list-item-subtitle>
+                            <v-list-item-subtitle>{{ address.city }} {{ address.zip }}</v-list-item-subtitle>
                         </v-list-item-content>
                     </v-list-item>
                 </div>
             </div>
-
-            <!-- </v-card-text> -->
         </v-card>
         <div v-else>
             <h1>You are not logged in.</h1>
@@ -61,12 +62,60 @@
                 firstName: '',
                 lastName: '',
                 userEmail: '',
+                MSISDNs: [],
 
                 loggedIn: false,
                 accountIDs: [], // save the account ids under user's account
                 getAccounts: [], // save each account ids' info using getAccounts API
                 loading: false,
             }
+        },
+        methods: {
+            getAccountInfo(element) {
+                // console.log('@@@ getAccountInfo @@@:', element)
+                return axios.post(
+                    'https://api-project9.lotusflare.com/api/v3/user/get_account',
+                    {
+                        target_id: element,
+                        target_type: 2,
+                    },
+                    {
+                        headers: {
+                            Authorization: localStorage.getItem('api_token'),
+                        },
+                    }
+                )
+            },
+            // FIXME: no test data for get_orders API
+            getOrderInfo(element) {
+                return axios.post(
+                    'https://api-project9.lotusflare.com/api/v3/order/get_orders',
+                    {
+                        target_id: element,
+                        target_type: 1,
+                    },
+                    {
+                        headers: {
+                            Authorization: localStorage.getItem('api_token'),
+                        },
+                    }
+                )
+            },
+            // FIXME: not test data for fetch_all_payment_methods API
+            getPaymentMethod(element) {
+                return axios.post(
+                    'https://api-project9.lotusflare.com/api/v3/payment/fetch_all_payment_methods',
+                    {
+                        target_id: element,
+                        target_type: 2,
+                    },
+                    {
+                        headers: {
+                            Authorization: localStorage.getItem('api_token'),
+                        },
+                    }
+                )
+            },
         },
         async mounted() {
             // post to get user info
@@ -97,22 +146,28 @@
                     })
                 // debugger
                 this.accountIDs.forEach((element) => {
-                    axios
-                        .post(
-                            'https://api-project9.lotusflare.com/api/v3/user/get_account',
-                            {
-                                target_id: element,
-                                target_type: 2,
-                            },
-                            {
-                                headers: {
-                                    Authorization: localStorage.getItem('api_token'),
-                                },
-                            }
-                        )
-                        .then((response) => {
-                            console.log(response.data)
-                            this.getAccounts.push(response.data)
+                    // axios
+                    //     .post(
+                    //         'https://api-project9.lotusflare.com/api/v3/user/get_account',
+                    //         {
+                    //             target_id: element,
+                    //             target_type: 2,
+                    //         },
+                    //         {
+                    //             headers: {
+                    //                 Authorization: localStorage.getItem('api_token'),
+                    //             },
+                    //         }
+                    //     )
+
+                    // FIXME: not call getOrderInfo and getPaymentMethod API yet
+                    Promise.all([this.getAccountInfo(element)])
+                        .then((responses) => {
+                            // 0 is account info
+                            // 1 is order info
+                            // 2 is payment method info
+                            console.log(responses[0].data)
+                            this.getAccounts.push(responses[0].data)
                         })
                         .catch((error) => {
                             console.log(error)
@@ -121,9 +176,9 @@
                             this.loading = false
                         })
                 })
+            } else {
+                this.loggedIn = false
             }
-            console.log('@this.accountIDs: ', this.accountIDs)
-            console.log('@this.getAccounts: ', this.getAccounts)
         },
     }
 </script>
