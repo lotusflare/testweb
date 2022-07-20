2e6cf31d2d7182602a8ddf59b19edf23cfb8664b
Qiance Yu <qiance.yu@lotusflare.com>
2022-07-19 23:51:39 -0700

    Beautify plans cards

--- src/components/PlansCards.vue
+++ src/components/PlansCards.vue
@@ -8,14 +8,25 @@
             </v-btn-toggle>
         </v-card-actions>
         <!-- <v-card-text> -->
-        <v-container fluid style="max-width: 1200px; position: relative;" class="white">
+        <v-container fluid style="max-width: 1200px; position: relative" class="white">
             <v-row>
                 <v-col cols="12" md="4" xs="12" v-for="(item, index) in plan" :key="index">
-                    <div class="black darken-2 text-center">
-                        <span class="white--text">{{ item.description }}</span>
-                    </div>
-                    <v-card height="800px" class="d-flex flex-column">
-                        <v-card-title>
+                    <v-sheet color="black" class="mt-10 darken-2 text-center" height="20" width="100%"> </v-sheet>
+                    <v-card height="800px" class="d-flex flex-column" tile>
+                        <v-sheet
+                            max-width="100"
+                            class="text-center rounded-circle mx-auto"
+                            style="margin-top: -64px; border: 5px solid #f01446"
+                            elevation=""
+                            height="100"
+                            width="100%"
+                            outlined
+                        >
+                            <div class="mt-11 red--text font-weight-black select-a-plan__best-value-text">
+                                {{ item.description }}
+                            </div>
+                        </v-sheet>
+                        <v-card-title class="mt-n4">
                             <v-col align="center">
                                 <div>
                                     {{ item.name }}
@@ -88,15 +99,27 @@
         methods: {
             getPlan(item) {
                 console.log(item)
-                this.$router.push({
-                    name: 'RwCheckout',
-                    query: {
-                        title: item.name,
-                        price: item.DISH_Customer_Price,
-                        // lines: item.lines,
-                        lines: item.num_of_lines,
-                    },
-                })
+                if (item.num_of_lines === 1) {
+                    this.$router.push({
+                        name: 'RwCheckout',
+                        query: {
+                            title: item.name,
+                            price: item.DISH_Customer_Price,
+                            lines: item.num_of_lines,
+                            entityID: item.entity_id,
+                        },
+                    })
+                } else {
+                    this.$router.push({
+                        name: 'RwCheckout',
+                        query: {
+                            title: item.name,
+                            price: item.DISH_Customer_Price * 2,
+                            lines: item.num_of_lines,
+                            entityID: item.entity_id,
+                        },
+                    })
+                }
             },
         },
         mounted() {
@@ -126,4 +149,11 @@
     }
 </script>
 
-<style scoped></style>
+<style scoped>
+    .select-a-plan__best-value-text {
+        line-height: 19px;
+        transform: translateY(-50%);
+        color: #f01446;
+        text-transform: uppercase;
+    }
+</style>
