34c1a84247bc5db37608d42577248046fe049fd2
hyouzan <qiance.yu@lotusflare.com>
2022-07-22 18:09:11 -0700

    hard code check zipcode

--- src/components/AddAddress.vue
+++ src/components/AddAddress.vue
@@ -15,9 +15,7 @@
         <v-alert type="success" v-if="success"> This address is covered </v-alert>
         <v-alert type="error" v-if="errorAddress"> This address is not covered </v-alert>
         <v-alert type="error" v-if="errorInternet"> Bad Request </v-alert>
-        <v-btn class="mr-4 mb-2" color="black" @click="addressContinue" :dark="continueBtn" :disabled="!continueBtn">
-            Continue
-        </v-btn>
+        <v-btn class="mr-4 mb-2" color="black" @click="addressContinue" dark> Continue </v-btn>
         <v-btn class="mb-2" @click="clear"> clear </v-btn>
     </v-form>
 </template>
@@ -51,49 +49,64 @@
             checkZipCode() {
                 //check the zip code is covered by the plan
                 this.zipLoading = true
-                axios
-                    .post(
-                        '/dishapi/coverageCheck',
-                        {
-                            zipCode: this.zip,
-                            requestType: 'Activation',
-                        },
-                        {
-                            headers: {
-                                dealerCode: 'DUBL1',
-                                'Content-Type': 'application/json',
-                                Accept: 'application/json',
-                                Authorization: 'Bearer ' + '',
-                            },
-                        }
-                    )
-                    .then((response) => {
-                        console.log(response.data)
-                        if (response.data.status === 'Success') {
-                            this.success = true
-                            this.continueBtn = true
-                            this.errorInternet = false
-                            this.errorAddress = false
-                            // TODO: send serviceAreas to NumberSelection
-                        }
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                        if (error.response.status === 417) {
-                            this.success = false
-                            this.continueBtn = false
-                            this.errorInternet = false
-                            this.errorAddress = true
-                        } else {
-                            this.success = false
-                            this.continueBtn = false
-                            this.errorInternet = true
-                            this.errorAddress = false
-                        }
-                    })
-                    .finally(() => {
-                        this.zipLoading = false
-                    })
+                // axios
+                //     .post(
+                //         '/dishapi/coverageCheck',
+                //         {
+                //             zipCode: this.zip,
+                //             requestType: 'Activation',
+                //         },
+                //         {
+                //             headers: {
+                //                 dealerCode: 'DUBL1',
+                //                 'Content-Type': 'application/json',
+                //                 Accept: 'application/json',
+                //                 Authorization: 'Bearer ' + '',
+                //             },
+                //         }
+                //     )
+                //     .then((response) => {
+                //         console.log(response.data)
+                //         if (response.data.status === 'Success') {
+                //             this.success = true
+                //             this.continueBtn = true
+                //             this.errorInternet = false
+                //             this.errorAddress = false
+                //             // TODO: send serviceAreas to NumberSelection
+                //         }
+                //     })
+                //     .catch((error) => {
+                //         console.log(error)
+                //         if (error.response.status === 417) {
+                //             this.success = false
+                //             this.continueBtn = false
+                //             this.errorInternet = false
+                //             this.errorAddress = true
+                //         } else {
+                //             this.success = false
+                //             this.continueBtn = false
+                //             this.errorInternet = true
+                //             this.errorAddress = false
+                //         }
+                //     })
+                //     .finally(() => {
+                //         this.zipLoading = false
+                //     })
+
+                // return success if zip codes are [27546, 28301, 28689]
+                if (this.zip === '27546' || this.zip === '28301' || this.zip === '28689') {
+                    this.success = true
+                    this.continueBtn = true
+                    this.errorInternet = false
+                    this.errorAddress = false
+                    this.zipLoading = false
+                } else {
+                    this.success = false
+                    this.continueBtn = false
+                    this.errorInternet = false
+                    this.errorAddress = true
+                    this.zipLoading = false
+                }
             },
             addressContinue() {
                 // send address data to father component "CheckoutSteppers"
