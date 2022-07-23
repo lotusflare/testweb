3841fe220733277a6ff36bca0871f0404b3f1bf8
hyouzan <qiance.yu@lotusflare.com>
2022-07-22 18:08:21 -0700

    hard code the numbers

--- src/components/NumberSelection.vue
+++ src/components/NumberSelection.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
-        <div class="text-h6 active mb-0 transition-swing" v-text="text"></div>
-        <v-select :items="items" label="Numbers"></v-select>
-        <v-btn class="mr-4" color="black" @click="getNumber" dark> Get number </v-btn>
-        <v-btn class="mr-0" color="black" @click="submit" dark> continue </v-btn>
+        <!-- <div class="text-h6 active mb-0 transition-swing" v-text="text"></div> -->
+        <v-select
+            :items="items"
+            label="Numbers"
+            v-model="numberData.number"
+            v-if="getNumberClicked === true"
+        ></v-select>
+        <v-btn class="mr-4 mb-4" color="black" @click="getNumber" dark> Get number </v-btn>
+        <v-btn class="mr-0 mb-4" color="black" @click="submit" dark> continue </v-btn>
     </div>
 </template>
 
@@ -12,28 +17,20 @@
         name: 'NumberSelection',
         data() {
             return {
-                text: 'Number Selection',
-                items: [
-                    { text: '1' },
-                    { text: '2' },
-                    { text: '3' },
-                    { text: '4' },
-                    { text: '5' },
-                    { text: '6' },
-                    { text: '7' },
-                    { text: '8' },
-                    { text: '9' },
-                    { text: '10' },
-                ],
+                // text: 'Number Selection',
+                items: [{ text: '9105147869' }, { text: '9105147870' }, { text: '9105147871' }],
+                getNumberClicked: false,
+                numberData: {
+                    number: '',
+                },
             }
         },
         methods: {
             getNumber() {
-                // TODO: get number from API
-                // FIXME: cannot get number from getAvailableMsisdn API
+                this.getNumberClicked = true
             },
             submit() {
-                this.$bus.$emit('number-data', this.items)
+                this.$bus.$emit('number-data', this.numberData)
                 this.$bus.$emit('stepper-next')
             },
         },
